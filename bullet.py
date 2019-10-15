f7555eddc7dbc95207917bd2c3aefcce3c5e92fe
Yasmin Maria Muniz de Oliveira <ymmdo.snf19@uea.edu.br>
2019-10-14 21:50:43 -0400

    Correção da angulação dos projéteis, mas eles ficaram meio lentos...

--- bullet.py
+++ bullet.py
@@ -12,15 +12,14 @@ shot_two_list = []
 def create_shooter_one():
     shot_one = draw('square', 0.16, '#2441a1',
                     tank.one.xcor(), tank.one.ycor())
+    shot_one.setheading(tank.one.heading())
+    shot_one.forward(20)
     shot_one_list.append(shot_one)
 
 
 def shooting_one():
-    x = 0
-    y = 0
-    for angle in tank.direction:
-        y = sin(radians(angle)) * 15
-        x = cos(radians(angle)) * 15
+    x = cos(radians(tank.one.heading()))
+    y = sin(radians(tank.one.heading()))
     shot_one_list[-1].dx = x
     shot_one_list[-1].dy = y
 
@@ -34,15 +33,14 @@ def shooter_one():
 def create_shooter_two():
     shot_two = draw('square', 0.16, '#a83232',
                     tank.two.xcor(), tank.two.ycor())
+    shot_two.setheading(tank.two.heading())
+    shot_two.forward(20)
     shot_two_list.append(shot_two)
 
 
 def shooting_two():
-    x = 0
-    y = 0
-    for angle in tank.direction:
-        y = sin(radians(angle)) * 15
-        x = cos(radians(angle)) * 15
+    x = cos(radians(tank.two.heading()))
+    y = sin(radians(tank.two.heading()))
     shot_two_list[-1].dx = x
     shot_two_list[-1].dy = y
 
