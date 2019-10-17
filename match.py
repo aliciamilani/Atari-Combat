2b9b3b86e22fd9e7b77f927c8669b71d0f744d4b
Yasmin Maria Muniz de Oliveira <ymmdo.snf19@uea.edu.br>
2019-10-16 20:48:42 -0400

    Correção do main.py.

--- match.py
+++ match.py
@@ -48,6 +48,15 @@ def create_zone(zone):
         y -= 16.3
 
 
+# criação do painel da pontuação
+hud_score = support.draw('square', 1, 'white', 0, 250)
+hud_score.hideturtle()
+hud_score.penup()
+hud_score.write('0 : 0', align='center', font=('Press Start 2P', 24, 'normal'))
+score_1 = 0
+score_2 = 0
+
+
 # interação entre os elementos do jogo
 def char_interplay():
 
@@ -58,17 +67,31 @@ def char_interplay():
         if tank.two.distance(wall_list[block]) <= 25:
             tank.two.forward(-20)
 
-    # colisão projétil 1 com o tanque 2
+    global score_1
+    global score_2
+    # colisão do projétil 1 com o tanque 2
     for ind_1 in bullet.shot_one_list:
         if (tank.two.distance(ind_1)) <= 25:
+            if ind_1.isvisible():
+                score_1 += 1
             ind_1.hideturtle()
+            ind_1.goto(500, 500)
             del(ind_1)
+            hud_score.clear()
+            hud_score.write('{} : {}'.format(score_1, score_2), align='center', font=(
+                            'Press Start 2P', 24, 'normal'))
 
-    # colisão projétil 2 com o tanque 1
+    # colisão do projétil 2 com o tanque 1
     for ind_2 in bullet.shot_two_list:
         if (tank.one.distance(ind_2)) <= 25:
+            if ind_2.isvisible(): 
+                score_2 += 1
             ind_2.hideturtle()
+            ind_2.goto(-500, 500)
             del(ind_2)
+            hud_score.clear()
+            hud_score.write('{} : {}'.format(score_1, score_2), align='center', font=(
+                            'Press Start 2P', 24, 'normal'))
 
     # colisão do projétil 1 com o labirinto
     for proj_1 in bullet.shot_one_list:
@@ -87,15 +110,15 @@ def char_interplay():
                 break
 
     # movimentação do projétil 1
-    for block in range(len(bullet.shot_one_list)):
-        bullet.shot_one_list[block].setx(bullet.shot_one_list[block].xcor() +
-                                         bullet.shot_one_list[block].dx)
-        bullet.shot_one_list[block].sety(bullet.shot_one_list[block].ycor() +
-                                         bullet.shot_one_list[block].dy)
+    for shot in range(len(bullet.shot_one_list)):
+        bullet.shot_one_list[shot].setx(bullet.shot_one_list[shot].xcor() +
+                                         bullet.shot_one_list[shot].dx)
+        bullet.shot_one_list[shot].sety(bullet.shot_one_list[shot].ycor() +
+                                         bullet.shot_one_list[shot].dy)
 
     # movimentação do projétil 2
-    for block in range(len(bullet.shot_two_list)):
-        bullet.shot_two_list[block].setx(bullet.shot_two_list[block].xcor() +
-                                         bullet.shot_two_list[block].dx)
-        bullet.shot_two_list[block].sety(bullet.shot_two_list[block].ycor() +
-                                         bullet.shot_two_list[block].dy)
+    for shot in range(len(bullet.shot_two_list)):
+        bullet.shot_two_list[shot].setx(bullet.shot_two_list[shot].xcor() +
+                                         bullet.shot_two_list[shot].dx)
+        bullet.shot_two_list[shot].sety(bullet.shot_two_list[shot].ycor() +
+                                         bullet.shot_two_list[shot].dy)
